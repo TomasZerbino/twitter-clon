9e6ca7017a54a25afac5d4121d77070b0930dc76
fgomensoro <102269369+fgomensoro@users.noreply.github.com>
2022-10-13 18:25:29 -0300

    additions to Tweet and Users models

--- models/Tweet.js
+++ models/Tweet.js
@@ -1,9 +1,16 @@
 const { mongoose, Schema } = require("../dbInitialSetup");
 
-const tweetSchema = new Schema({
-  content: String,
-  likes: Number,
-});
+const tweetSchema = new Schema(
+  {
+    content: String,
+    // likes: [ObjectId],
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true },
+);
 
 const Tweet = mongoose.model("Tweet", tweetSchema);
 
--- models/User.js
+++ models/User.js
@@ -1,16 +1,25 @@
 const { mongoose, Schema } = require("../dbInitialSetup");
 
-const userSchema = new Schema({
-  firstname: String,
-  lastname: String,
-  email: String,
-  username: String,
-  password: String,
-  tweets: {
-    type: Schema.Types.ObjectId,
-    ref: "Tweet",
+const userSchema = new Schema(
+  {
+    firstname: String,
+    lastname: String,
+    username: String,
+    email: String,
+    avatar: String,
+    password: String,
+    bio: String,
+    tweets: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Tweet",
+      },
+    ],
+    // followers: [ObjectId],
+    // following: [ObjectId],
   },
-});
+  { timestamps: true },
+);
 
 const User = mongoose.model("User", userSchema);
 
