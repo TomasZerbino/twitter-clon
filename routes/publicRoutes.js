a66947cfc0bac7cba5dc3e15e1a81ce09dfb2b86
TomasZerbino <103967982+TomasZerbino@users.noreply.github.com>
2022-10-17 15:17:07 -0300

    se agrega vista home y estilos

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const publicRouter = express.Router();
 const userController = require("../controllers/userController");
+const pagescontroller = require("../controllers/pagesController");
 
 // Rutas Públicas:
 // ...
+publicRouter.get("/", pagescontroller.showHome);
 
-publicRouter.get("/login", userController.login)
+publicRouter.get("/login", userController.login);
 
-publicRouter.get("/register", userController.register)
+publicRouter.get("/register", userController.register);
 
 publicRouter.get("/user/create", userController.create);
-
-
 
 module.exports = publicRouter;
