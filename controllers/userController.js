a3aa4cafbbbabe11159c2f7c9b7b3dcb31ea6cb2
fgomensoro <102269369+fgomensoro@users.noreply.github.com>
2022-11-17 17:33:13 -0300

    Feat: up to date

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,31 +14,7 @@ async function show(req, res) {}
 
 // Show the form for creating a new resource
 async function create(req, res) {
-  // const userAutentication = await User.findOne({ email: req.body.email });
-  // const passwordAutentication = req.body.password === req.body.confirmPassword;
-  // if (!userAutentication & passwordAutentication) {
-  //   const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  //   const userCreated = await User.create({
-  //     firstname: req.body.firstname,
-  //     lastname: req.body.lastname,
-  //     email: req.body.email,
-  //     username: req.body.username,
-  //     password: hashedPassword,
-  //   });
-  //   if (userCreated) {
-  //     req.login(userCreated, function () {
-  //       res.redirect("/login");
-  //     });
-  //   }
-  // } else {
-  //   if (!passwordAutentication) {
-  //     req.flash("user", "⚠️  Password confirmation doesn't match Password!");
-  //     res.redirect("back");
-  //   } else {
-  //     req.flash("user", "⚠️  User already exists!");
-  //     res.redirect("back");
-  //   }
-  // }
+
 }
 // Update the specified resource in storage.
 async function update(req, res) {
